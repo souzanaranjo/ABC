5b372715c5eb2a3547aabed046a628c478a04e7f
David Souza <souzanaranjo@gmail.com>
2018-04-29 18:26:54 -0500

    Hicimos division en varias pestanas y agregamos informacion, varia info de empresa y la info de los procesos

--- js/index.js
+++ js/index.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
-	var empresas = [
+	menuFunctionality();
+	initMap();
+});
+
+function menuFunctionality(){
+    $("#navprincipal > li").on("click",function(){
+        $(".selected").removeClass("selected");
+        $(".active").removeClass("active");
+        var $currentSelection = $(".currentSelection");
+        $currentSelection.removeClass("currentSelection");
+        $currentSelection.addClass("hiddenSection");
+
+        var currentSection = $(this).attr("class");
+        $("#" + currentSection).addClass("currentSelection").removeClass("hiddenSection");
+        $(this).addClass("selected");
+        $(this).addClass("active");
+    });
+}
+
+function initMap() {
+        var monterrey = {lat: 25.686613, lng: -100.316116};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: monterrey
+        });
+        return map;
+}
+
+function graficaFabricacion() {
+	map = initMap();
+	var Fabricacion = [
 	{
 		nombre: "MIMSA",
 		direccion: "Carretera Miguel Aleman, Bodega 8 y 5, CP 66633 Apodaca, N.L.",
@@ -15,32 +45,35 @@ $(document).ready(function(){
 		pagina: 'https://www.eficaingenieria.com/',
 		lat: 25.680832,
 		lng: -100.325665
+	},
+	{
+		nombre: "MYDSA Procesos",
+		direccion: "Dirección: Arteaga No. 2111 Oriente, Col. Obrera,, Monterrey, N.L. 64010",
+		telefono: '01 81 8355 0967',
+		pagina: 'http://www.mydsaprocesos.com/',
+		lat: 25.681953,
+		lng: -100.293930
+	},
+	{
+		nombre: "Metalizados y Maquinados de México S.A. de C.V.",
+		direccion: "Joaquín G. Leal Nte. No. 646, Zona Centro, Monterrey, N.L. 64000",
+		telefono: '01 81 8374 1541',
+		pagina: 'http://www.mymmsa.com.mx/',
+		lat: 25.659439,
+		lng: -100.284272
 	}
 	];
-	initMap(empresas);
-});
-
-function initMap(empresas) {
-        var monterrey = {lat: 25.686613, lng: -100.316116};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: monterrey
-        });
-        graficaEmpresas(empresas, map);
-}
-
-function graficaEmpresas(empresas, map) {
 	var marker = [];
-	for(var i = 0; i < empresas.length; i++) {
+	for(var i = 0; i < Fabricacion.length; i++) {
 		marker[i] = new google.maps.Marker({
-                    position : new google.maps.LatLng(empresas[i].lat, empresas[i].lng),
+                    position : new google.maps.LatLng(Fabricacion[i].lat, Fabricacion[i].lng),
                     map: map
 		});
 
 		google.maps.event.addListener(marker[i], 'click', (function(marker, i) {
                     return function(){
                     	$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-                        empresa = empresas[i];
+                        empresa = Fabricacion[i];
                         var infoEmpresa = getInfoEmpresa(empresa);
                         $("#modalText").empty().append(infoEmpresa);
                         $("#headerModal").empty().append(empresa.nombre);
