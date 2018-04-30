1d37c658aa585eea4f6b7efff11b8e4666a426ef
David Souza <souzanaranjo@gmail.com>
2018-04-29 20:04:50 -0500

    Almost finished

--- js/index.js
+++ js/index.js
@@ -3,6 +3,38 @@ $(document).ready(function(){
 	initMap();
 });
 
+function calcularpm() {
+	var diametro = $("#diametroa").val();
+	var vc = $("#inputvc").val();
+
+	var res = (1000 * vc) / (3.1416 * diametro);
+
+	$("#resultadoa").empty().append("Resultado = " + res.toFixed(2) + " revoluciones por minuto");
+}
+
+function calculavc() {
+	var diametrob = $("#diametrob").val();
+	var rpm = $("#inputrpm").val();
+
+	var resb = (3.1416 * diametrob * rpm) / 1000;
+
+	$("#resultadob").empty().append("Resultado = " + resb.toFixed(2) + " metros/minuto");
+}
+
+function rpm() {
+	$("#rpm").show();
+	$("#vc").hide();
+
+}
+
+
+
+function vc() {
+	$("#vc").show();
+	$("#rpm").hide();
+
+}
+
 function menuFunctionality(){
     $("#navprincipal > li").on("click",function(){
         $(".selected").removeClass("selected");
@@ -95,6 +127,144 @@ function graficaFabricacion() {
 	}
 }
 
+function graficaMaquinaria() {
+	map = initMap();
+	var Fabricacion = [
+		{
+			nombre: "Mazak",
+			direccion: "Spectrum 100, Parque Industrial Finsa, CP 66600, Apodaca, N.L.",
+			telefono: '01 81 8221 0910',
+			pagina: 'https://www.mazakusa.com/es/technology-centers/mexico/',
+			lat:25.789174,
+			lng: -100.141772
+		},	
+		{
+			nombre: "Hass",
+			direccion: "Boulevard Aeropuerto 4033, Parque Industrial Finsa, CP 66600, Apodaca, N.L.",
+			telefono: '01 81 8145 0473 ',
+			pagina: 'http://www.haas.com.mx/index.html',
+			lat:25.776907,
+			lng: -100.142559,
+		},	
+		{
+			nombre: "Famasa",
+			direccion: "Calle Gonzalitos 523, Mitras Nte., 64320 Monterrey, N.L.",
+			telefono: '01 81 8370 2192',
+			pagina: 'http://www.famasa.com/',
+			lat: 25.685344,
+			lng: -100.342068
+		},
+		{
+			nombre: "Viwa",
+			direccion: "Camino a las Cañadas 900-6C Bosque, Bosques de San Isidro, 45133 Zapopan",
+			telefono: '01 33 3633 4139',
+			pagina: 'http://www.viwacnc.com/',
+			lat: 20.748248,
+			lng: -103.385162
+		},	
+		{	
+			nombre: "DMG MORI",
+			direccion: "Calle Acceso III No 14 Bodega 11, Benito Juárez, 76089 Santiago de Querétaro, Querétaro",
+			telefono: '01 442 209 5072',
+			pagina: 'https://mx.dmgmori.com/',
+			lat: 20.630544,
+			lng: -100.435215,
+		}
+	];
+	var marker = [];
+	for(var i = 0; i < Fabricacion.length; i++) {
+		marker[i] = new google.maps.Marker({
+                    position : new google.maps.LatLng(Fabricacion[i].lat, Fabricacion[i].lng),
+                    map: map
+		});
+
+		google.maps.event.addListener(marker[i], 'click', (function(marker, i) {
+                    return function(){
+                    	$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+                        empresa = Fabricacion[i];
+                        var infoEmpresa = getInfoEmpresa(empresa);
+                        $("#modalText").empty().append(infoEmpresa);
+                        $("#headerModal").empty().append(empresa.nombre);
+                        var modal = document.getElementById('myModal');
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+                        modal.style.display = "block";
+                        span.onclick = function(){
+                            modal.style.display = "none";
+                        }
+                        // When the user clicks anywhere outside of the modal, close it
+                        window.onclick = function(event) {
+                            if (event.target == modal) {
+                                modal.style.display = "none";
+                            }
+                        } 
+                    }
+         })(marker[i], i));
+	}
+}
+
+
+
+function graficaHerramientas() {
+	map = initMap();
+	var Herramientas = [
+		{
+			nombre: "Herramental Monterrey",
+			direccion: "Prol. Madero 3852, Fierro, 64590 Monterrey, N.L.",
+			telefono: '01 81 8126 5150',
+			pagina: 'http://www.herramental.com.mx',
+			lat: 25.683573,
+			lng: -100.275230
+		},
+		{
+			nombre: "Travers Tools",
+			direccion: "Av. Alfonso Reyes No. 2823, Col. Del Prado, Monterrey, N.L. 64410",
+			telefono: '01 81 83 80 66 10',
+			pagina: '01 81 83 80 66 10',
+			lat: 25.703281,
+			lng: -100.315383
+		},	
+		{
+			nombre: "TecnoTroqueles",
+			direccion: "64500, Dolores Ladrón de Guevara 402, Del Nte., Monterrey, N.L.",
+			telefono: '01 81 8331 2441',
+			pagina: 'http://www.tecnotroqueles.com/espanol/index.html',
+			lat:25.712386,
+			lng: -100.306528
+		}
+	];
+	var marker = [];
+	for(var i = 0; i < Herramientas.length; i++) {
+		marker[i] = new google.maps.Marker({
+                    position : new google.maps.LatLng(Herramientas[i].lat, Herramientas[i].lng),
+                    map: map
+		});
+
+		google.maps.event.addListener(marker[i], 'click', (function(marker, i) {
+                    return function(){
+                    	$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+                        empresa = Herramientas[i];
+                        var infoEmpresa = getInfoEmpresa(empresa);
+                        $("#modalText").empty().append(infoEmpresa);
+                        $("#headerModal").empty().append(empresa.nombre);
+                        var modal = document.getElementById('myModal');
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+                        modal.style.display = "block";
+                        span.onclick = function(){
+                            modal.style.display = "none";
+                        }
+                        // When the user clicks anywhere outside of the modal, close it
+                        window.onclick = function(event) {
+                            if (event.target == modal) {
+                                modal.style.display = "none";
+                            }
+                        } 
+                    }
+         })(marker[i], i));
+	}
+}
+
 function getInfoEmpresa(empresa) {
 	var infoEmpresa =
 	'<div class="center">' +
@@ -104,5 +274,3 @@ function getInfoEmpresa(empresa) {
 	'</div>'
 	return infoEmpresa;
 }
-
-
